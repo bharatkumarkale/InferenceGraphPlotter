9c3b85ac0fc59f06c57984e315d3b928f6d0a80f
Bharat Kumar Kale <kale@anl.gov>
2024-07-26 08:29:35 -0500

    Fix for number formatting on Y-axis

--- src/js/display.js
+++ src/js/display.js
@@ -373,7 +373,7 @@ function display() {
 
     targetG.append('g')
         .attr("class", "y-axis")
-        .call(d3.axisLeft(y_scale).ticks(7))
+        .call(d3.axisLeft(y_scale).ticks(7).tickFormat(d3.format(".2s")))
     targetG.append('text')
         .attr('transform', `translate(${-90}, ${height/2}) rotate(-90)`)
         .attr('class', 'axisText')
